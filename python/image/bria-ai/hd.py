0d48a1df9db567b5459140cfc9668bf2c5dfe1b9
jyoung105 <jeongmin1604@gmail.com>
2025-02-28 22:15:05 +0900

    feat: add Bria AI HD image generation script

---
+++ python/image/bria-ai/hd.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+import fal_client
+import json
+import requests
+
+# Load the environment variables
+load_dotenv()
+os.environ["FAL_KEY"] = os.environ.get("FAL_CLIENT_ID")
+
+# Define the on_queue_update function
+def on_queue_update(update):
+    if isinstance(update, fal_client.InProgress):
+        for log in update.logs:
+           print(log["message"])
+
+prompts = json.load(open("python/prompt.json"))
+
+for i in range(len(prompts['prompts'])):
+    result = fal_client.subscribe(
+        "fal-ai/bria/text-to-image/hd",
+        arguments={
+            "prompt": prompts['prompts'][i]['prompt'],
+            "negative_prompt": "",
+            "num_images": 1,
+            "aspect_ratio": "1:1",
+            "seed": 42,
+            "num_inference_steps": 30,
+            "guidance_scale": 5,
+            "prompt_enhancement": True,
+        },
+        with_logs=True,
+        on_queue_update=on_queue_update,
+    )
+    
+    image = requests.get(result['images'][0]['url'])
+    with open(f"python/result/bria-ai/hd/{str(i)}.png", "wb") as f:
+        f.write(image.content)
