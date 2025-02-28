964ee0f41736cadb8911a058f89ebe74386a6718
jyoung105 <jeongmin1604@gmail.com>
2025-02-28 21:48:59 +0900

    feat: add Imagen-3 image generation scripts with fast and standard variants

---
+++ python/image/google/imagen-3.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+import fal_client
+import json
+import requests
+
+# Load the environment variables
+load_dotenv()
+os.environ["FAL_KEY"] = os.environ.get("FAL_CLIENT_ID")
+
+# Define the on_queue_update function
+def on_queue_update(update):
+    if isinstance(update, fal_client.InProgress):
+        for log in update.logs:
+           print(log["message"])
+
+prompts = json.load(open("python/prompt.json"))
+
+for i in range(len(prompts['prompts'])):
+    result = fal_client.subscribe(
+        "fal-ai/imagen3",
+        arguments={
+            "prompt": prompts['prompts'][i]['prompt'],
+            "negative_prompt": "",
+            "aspect_ratio": "1:1",
+            "num_images": 1,
+            "seed": 42,
+        },
+        with_logs=True,
+        on_queue_update=on_queue_update,
+    )
+    
+    image = requests.get(result['images'][0]['url'])
+    with open(f"python/result/google/imagen-3/{str(i)}.png", "wb") as f:
+        f.write(image.content)
